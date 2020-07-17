90bba22e106c69a60262f268928604c1fb16c88a
Ygor Gallina <ygor.gallina@gmail.com>
2020-07-17 13:23:38 +0200

    Cleaning plop space, scaled down portrait

--- plop/script.sh
+++ plop/script.sh
@@ -1,13 +1,14 @@
+set -e
 dirFile="dir.pug"
 
 # Prints the list of the directories to a file
 # used by index.pug
 echo -n "- var directories = [" > $dirFile
-for i in * ; do
-  test -d "$i" && test "$i" != "ressources" && test "$i" != "templates" && echo -n "\"$i\", "
+find . -maxdepth 1 -type d | grep -vi "p5\|ressources\|templates" | while read i ; do
+
+    echo -n "\"${i##*/}\", "
+
 done >> $dirFile
 echo -n "]" >> $dirFile
 
 pug index.pug
-
-(cd "Processing Experiments" ; sh script.sh)
