064b3480e1a79a6bcd82a2680d686d5c9043e058
Ygor Gallina <ygor.gallina@gmail.com>
2020-07-17 13:38:19 +0200

    Remove . in plop space*

--- plop/script.sh
+++ plop/script.sh
@@ -4,7 +4,7 @@ dirFile="dir.pug"
 # Prints the list of the directories to a file
 # used by index.pug
 echo -n "- var directories = [" > $dirFile
-find . -maxdepth 1 -type d | grep -vi "p5\|ressources\|templates" | while read i ; do
+find . -maxdepth 1 -type d | grep -vi "p5\|ressources\|templates\|\.$" | while read i ; do
 
     echo -n "\"${i##*/}\", "
 
